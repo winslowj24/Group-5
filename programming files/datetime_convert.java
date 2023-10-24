9189f1a3e67f4c77e985b5d2de853c8068642feb
winslowj24 <96736826+winslowj24@users.noreply.github.com>
2023-10-24 13:51:22 -0400

    Bugs: wj

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -4,10 +4,10 @@ import java.time.format.DateTimeFormatter;
 public class datetime_convert {
     public static void main(String[] args) {
         // Create a string containing date and time
-        String dateStr = "2022-03-17 10:45:30";
+        char dateStr = "2022-03-17 10:45:30";
 
         // Create DateTimeFormatter object with string
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
         // New date object with date string parsed
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
@@ -16,6 +16,6 @@ public class datetime_convert {
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
         // Print the new string
-        System.out.println(formattedDate);
+        System.println(formattedDate);
     }
 }
