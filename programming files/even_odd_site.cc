d8e396a0c476585ba4353244d3788eb44cdcc4a2
kaipasciak <123124121+kaipasciak@users.noreply.github.com>
2023-10-24 13:54:56 -0400

    Update even_odd_site.cc
    Added bugs

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
     // Open file
-    ofstream file;
+    
     file.open("numbers.html");
     // Adding HTML to file
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
 
     // Add table to file line by line
-    for (int i = 1; i <= 50; i++) {
+    for (i = 1; i <= 50; i++) {
         // If i is even, put i in first cell
-        if (i % 2 == 0) {
+        if (i % 2 = 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
         // If i is odd, put i in second cell
