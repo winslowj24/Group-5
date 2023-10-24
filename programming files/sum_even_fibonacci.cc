b1c6b54becd7100a4464ebfef5f3ce5ba63deca4
Nairarj <123185453+Nairarj@users.noreply.github.com>
2023-10-24 13:42:58 -0400

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -2,9 +2,13 @@
 
 int main()
 {
+    //Initializes variables a and b as integers
     int a = 0, b = 1;
+    //sum_even variable is declared as integer 0
     int sum_even = 0;
+    //While loop 
     while (b < 4000000) {
+        //if b is even then add it to sum_even else set temp to b and set b to be the sum of a and b, set a to temp.
         if (b % 2 == 0) {
             sum_even += b;
         }
@@ -12,6 +16,7 @@ int main()
         b = a + b;
         a = temp;
     }
+    //Print sum_even variable
     std::cout << sum_even << std::endl;
 
     return 0;
