b81b0666bfec72f8c34f8d42da173fee1d1db9e8
Nairarj <123185453+Nairarj@users.noreply.github.com>
2023-10-24 13:54:38 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -3,16 +3,16 @@ with open("numbers.html", "w") as f:
     # Adding HTML to file
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
-    # Add table to file line by line
-    for i in range(1, 50):
+    # Add table to file line by line, bug introduced: A.N
+    for (i = 0, i < 50, i--):
         # If i is even, put i in first cell
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         # If i is odd, put i in second cell
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
-    # End table tag
-    f.write("</table>\n</body>\n</html>")
+    # End table tag, bug introduced: A.N.
+    f.string("</table>\n</body>\n</html>")
 
 with open("numbers.html", "r") as f:
     print(f.read())
