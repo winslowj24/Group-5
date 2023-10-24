8e1c933f6237d064f68601ec7c09994d594cea2f
Project1-wfoot <wfoot@uvm.edu>
2023-10-24 13:54:00 -0400

    bugs remove dupe

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class remove_duplicates {
 
-    public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
-        List<Integer> uniqueArr = new ArrayList<>();
+    public static List<string> removeDuplicates(List<string> arr, List<string> removedElements) {
+        List<string> uniqueArr = new ArrayList<>();//bug introduced wfoot
         for (int i = 0; i < arr.size(); i++) {
             if (!uniqueArr.contains(arr.get(i))) {
                 uniqueArr.add(arr.get(i));
@@ -20,7 +20,7 @@ public class remove_duplicates {
         // Test the function
         List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
         List<Integer> removedElements = new ArrayList<>();
-        List<Integer> uniqueArr = removeDuplicates(arr, removedElements);
+        List<Integer> uniqueArr = removeDuplicates(removedElements,arr); // introduced bug wfoot
         System.out.println("Original array: " + arr);
         System.out.println("Unique array: " + uniqueArr);
         System.out.println("Removed elements: " + removedElements);
--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -3,7 +3,7 @@ def remove_duplicates(arr):
     unique_arr = []
     removed_elements = []
     for i in range(len(arr)):
-        if i == 0 or arr[i] != arr[i-1]:
+        if i == 0 or arr[i] != arr[i+1]:#bug introduced wfoot
             unique_arr.append(arr[i])
         else:
             removed_elements.append(arr[i])
@@ -12,6 +12,7 @@ def remove_duplicates(arr):
 # Test the function
 arr = [1, 2, 2, 3, 4, 4, 5]
 unique_arr, removed_elements = remove_duplicates(arr)
-print("Original array:", arr)
-print("Unique array:", unique_arr)
-print("Removed elements:", removed_elements)
+cout << ("Original array:", arr) << endl;
+cout << ("Unique array:", unique_arr)<< endl;
+cout << ("Removed elements:", removed_elements)<< endl;
+#bug introduced wfoot
