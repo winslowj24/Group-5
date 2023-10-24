592ec692227dd1b2673ea4503cd9878610e837ad
Nairarj <123185453+Nairarj@users.noreply.github.com>
2023-10-24 14:15:05 -0400

    Bug Report datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -3,11 +3,11 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        // Create a string containing date and time
-        char dateStr = "2022-03-17 10:45:30";
+        // Create a string containing date and time, BUG FIXED: wrong type
+        String dateStr = "2022-03-17 10:45:30";
 
-        // Create DateTimeFormatter object with string
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        // Create DateTimeFormatter object with string, BUG FIXED: missing semi-colon
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         // New date object with date string parsed
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
@@ -15,7 +15,7 @@ public class datetime_convert {
         // Create new string from the date object
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
-        // Print the new string
-        System.println(formattedDate);
+        // Print the new string, BUG FIXED: missing out
+        System.out.println(formattedDate);
     }
 }
