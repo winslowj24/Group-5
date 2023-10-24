6a625fba7eb5599826ded04367315415dc7e5f7b
Nairarj <123185453+Nairarj@users.noreply.github.com>
2023-10-24 13:48:01 -0400

    Update sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,15 +1,20 @@
 public class sum_even_fibonacci {
     public static void main(String[] args) {
+     //Initializes variables a and b as integers
         int a = 0, b = 1;
+        //Intializes sum_even variable
         int sumEven = 0;
+        //while loop loops until b is even 
         while (b < 4000000) {
             if (b % 2 == 0) {
                 sumEven += b;
             }
+            //Set temp variable to b, b is the sum of a and b, and set a to temp
             int temp = b;
             b = a + b;
             a = temp;
         }
+        //Print sum even variable
         System.out.println(sumEven);
     }
 }
