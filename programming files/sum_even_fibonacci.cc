6eb43ca59a177076752155d61ff9ebf7aafebba1
Project1-wfoot <wfoot@uvm.edu>
2023-10-24 14:11:24 -0400

    bugs fixed on fibonacci

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -9,15 +9,15 @@ int main()
     //sum_even variable is declared as integer 0
     int sum_even = 0;
     //While loop 
-    while (b > 4000000) {
+    while (b < 4000000) {
         //if b is even then add it to sum_even else set temp to b and set b to be the sum of a and b, set a to temp.
         if (b % 2 == 0) {
             sum_even += b;
+        } else {
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
-        int temp = b;
-        b = a + b;
-        a = temp;
-        
         //Print sum_even variable
         std::cout << sum_even << std::endl;
     }
