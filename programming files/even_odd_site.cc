313d00e4390f89d15078dc6f2c78cabd787240e8
winslowj24 <96736826+winslowj24@users.noreply.github.com>
2023-10-24 13:39:38 -0400

    Update even_odd_site.cc
    cc comments

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -3,19 +3,27 @@
 using namespace std;
 
 int main() {
+    // Open file
     ofstream file;
     file.open("numbers.html");
+    // Adding HTML to file
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+
+    // Add table to file line by line
     for (int i = 1; i <= 50; i++) {
+        // If i is even, put i in first cell
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
+        // If i is odd, put i in second cell
         else {
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
     }
+    // End table tag
     file << "</table>\n</body>\n</html>";
+    // Close file
     file.close();
     ifstream input("numbers.html");
     cout << input.rdbuf();
