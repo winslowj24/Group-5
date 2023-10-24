39a1ebd2c5a04de0d5e21eb383d3062a51d4a6b1
Nairarj <123185453+Nairarj@users.noreply.github.com>
2023-10-24 14:17:50 -0400

    Bug Report datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-# Define string containing date and time in compatible format with date time object
-date_str = "2022-03-17 10:45:30
+# Define string containing date and time in compatible format with date time object, BUG FIXED: missing apostrophe
+date_str = "2022-03-17 10:45:30"
 
-# Create date time object by parsing the string
-date_obj = datetime.strptime(date_str '%Y-%m-%d %H:%M:%S')
+# Create date time object by parsing the string, BUG FIXED missing comma
+date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 
-# Convert the date time object to a string
-formatted_date = date_obj.strftime('m/d/Y H:M:S')
+# Convert the date time object to a string, BUG FIXED: missing modulus'
+formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
 # Print date time object
 print(formatted_date)
