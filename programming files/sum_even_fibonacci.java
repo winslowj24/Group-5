c11cc680edf8670565f761de3ea64627a9435984
mckenziekelley <123017721+mckenziekelley@users.noreply.github.com>
2023-10-24 13:55:21 -0400

    added bugs

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,3 +1,5 @@
+// bugs added by mckenzie (3 bugs)
+
 public class sum_even_fibonacci {
     public static void main(String[] args) {
      //Initializes variables a and b as integers
@@ -6,8 +8,8 @@ public class sum_even_fibonacci {
         int sumEven = 0;
         //while loop loops until b is even 
         while (b < 4000000) {
-            if (b % 2 == 0) {
-                sumEven += b;
+            if (b // 2 == 0) {
+                sumEven -= b;
             }
             //Set temp variable to b, b is the sum of a and b, and set a to temp
             int temp = b;
@@ -15,7 +17,7 @@ public class sum_even_fibonacci {
             a = temp;
         }
         //Print sum even variable
-        System.out.println(sumEven);
+        std::cout << sumEven << endl;
     }
 }
 
